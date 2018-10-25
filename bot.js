6130bac87899a6ed5c141363b75a14e861afff07
DON15NINO <44461351+DON15NINO@users.noreply.github.com>
2018-10-25 20:40:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -676,3 +676,11 @@ client.on('message', function(message) {
     }
     }
 })
+client.on('typingStart', (ch, user) => {
+    if(user.presence.status === 'offline') {
+        ch.send(`${user} تحذير هذا شخص مسوي نفسه اوف لاين ويكتب`)
+        .then(msg => {
+            msg.delete(10000)
+        })
+    }
+});
