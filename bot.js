79ac2efac87bb1d2738bb1b4190df82e23155d84
DON15NINO <44461351+DON15NINO@users.noreply.github.com>
2018-10-26 22:45:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,49 +7,6 @@ client.on('ready', () => {
   console.log('---------------')
 });
 
-
-client.on('message', message => {
-        var prefix = "^";
-              if(!message.channel.guild) return;
-    if(message.content.startsWith(prefix + 'bc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "๖̶̶̶ζ͜͡¤ĞămĕŘ&proo👑";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-    
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-    
-    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
-    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
- var bc = new
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setTitle('رسالة جديده  :loudspeaker: ')
-       .addField('السيرفر :sparkles: :', message.guild.name)
-       .addField('المرسل :grey_exclamation: :', message.author.username)
-       .addField('الرساله :envelope: :', args)
-       .setThumbnail(message.author.avatarURL)
-       .setFooter(copy, client.user.avatarURL);
-    m.send({ embed: bc })
-    msg.delete();
-    })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
-    }
-    });
 client.login('NTA0OTI2ODg4OTI2MzgwMDMy.DrMKQw.oKN9qJvCfV1wHo9e3DrsmS8YrGI');
 	client.on('message' , message => {
 if (message.content === '^owner') {
@@ -583,7 +540,7 @@ Dat = currentTime.getDate()//Alpha Codes
   }
  
 });
-const adminprefix = "^";
+const adminprefix = "$";
 const devs = ['430860058591756289','480184422386237451'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
